f84b1a6603b3f7c8bfb06c6c1f0143c7acea8161
Nickson Ogm <nicksonogm@gmail.com>
2025-08-14 18:00:33 +0300

    safety

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -73,57 +73,51 @@ function HeroSection() {
           </div>
         </div>
 
-        {/* RIGHT: Animated Gradient SVG (contained) */}
-        <div className="h-full flex items-center justify-end pr-6 md:pr-16">
-          {/* Hard mask to prevent spillover on scale */}
-          <div className="w-[220px] h-[220px] md:w-[260px] md:h-[260px] overflow-hidden">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 248 248"
-              width="100%"
-              height="100%"
-              aria-hidden="true"
-              className="block"
-              preserveAspectRatio="xMidYMid meet"
-            >
-              <defs>
-                <linearGradient
-                  id="paint0_linear_2452_11835"
-                  x1="218"
-                  x2="-47.283"
-                  y1="258"
-                  y2="153.706"
-                  gradientUnits="userSpaceOnUse"
-                >
-                  <stop offset=".27" stopColor="#FFD3FD" />
-                  <stop offset=".838" stopColor="#806EFF" />
-                </linearGradient>
-                <linearGradient
-                  id="paint1_linear_2452_11835"
-                  x1="-21.183"
-                  x2="223.712"
-                  y1="-7.807"
-                  y2="329.472"
-                  gradientUnits="userSpaceOnUse"
-                >
-                  <stop offset=".27" stopColor="#FEC5FB" />
-                  <stop offset=".838" stopColor="#00BAE2" />
-                </linearGradient>
-              </defs>
-
-              {/* Animate this group, not the <svg> canvas */}
-              <g id="turbine-shape" style={{ transformBox: "fill-box" }}>
-                <path
-                  fill="url(#paint0_linear_2452_11835)"
-                  d="M152.266 123.716h94.275c.802 0 1.459.656 1.459 1.459v121.067c0 .81-.664 1.474-1.474 1.466-67.274-.78-121.669-55.137-122.522-122.387v121.22c0 .803-.657 1.459-1.46 1.459H1.474c-.81 0-1.474-.664-1.467-1.474C.795 178.721 56 124.008 123.996 124H1.459C.657 124 0 123.344 0 122.541V1.474C0 .664.664 0 1.474.008c67.274.78 121.669 55.137 122.522 122.387V1.46c0-.803.657-1.46 1.46-1.46h121.07c.81 0 1.474.664 1.467 1.474-.679 58.224-41.486 106.801-96.055 119.367-1.686.386-1.401 2.875.336 2.875h-.008Z"
-                />
-                <path
-                  fill="url(#paint1_linear_2452_11835)"
-                  d="M152.266 123.716h94.275c.802 0 1.459.656 1.459 1.459v121.067c0 .81-.664 1.474-1.474 1.466-67.274-.78-121.669-55.137-122.522-122.387v121.22c0 .803-.657 1.459-1.46 1.459H1.474c-.81 0-1.474-.664-1.467-1.474C.795 178.721 56 124.008 123.996 124H1.459C.657 124 0 123.344 0 122.541V1.474C0 .664.664 0 1.474.008c67.274.78 121.669 55.137 122.522 122.387V1.46c0-.803.657-1.46 1.46-1.46h121.07c.81 0 1.474.664 1.467 1.474-.679 58.224-41.486 106.801-96.055 119.367-1.686.386-1.401 2.875.336 2.875h-.008Z"
-                />
-              </g>
-            </svg>
-          </div>
+        {/* RIGHT: Animated Gradient SVG */}
+        <div className="w-[280px] md:w-[320px] h-full flex items-center justify-end pr-6 md:pr-16 overflow-hidden">
+          <svg
+            id="turbine-shape"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 248 248"
+            width="220"
+            height="220"
+            aria-hidden="true"
+            className="transform"
+          >
+            <path
+              fill="url(#paint0_linear_2452_11835)"
+              d="M152.266 123.716h94.275c.802 0 1.459.656 1.459 1.459v121.067c0 .81-.664 1.474-1.474 1.466-67.274-.78-121.669-55.137-122.522-122.387v121.22c0 .803-.657 1.459-1.46 1.459H1.474c-.81 0-1.474-.664-1.467-1.474C.795 178.721 56 124.008 123.996 124H1.459C.657 124 0 123.344 0 122.541V1.474C0 .664.664 0 1.474.008c67.274.78 121.669 55.137 122.522 122.387V1.46c0-.803.657-1.46 1.46-1.46h121.07c.81 0 1.474.664 1.467 1.474-.679 58.224-41.486 106.801-96.055 119.367-1.686.386-1.401 2.875.336 2.875h-.008Z"
+            />
+            {/* Pattern overlay removed for self‑contained rendering */}
+            <path
+              fill="url(#paint1_linear_2452_11835)"
+              d="M152.266 123.716h94.275c.802 0 1.459.656 1.459 1.459v121.067c0 .81-.664 1.474-1.474 1.466-67.274-.78-121.669-55.137-122.522-122.387v121.22c0 .803-.657 1.459-1.46 1.459H1.474c-.81 0-1.474-.664-1.467-1.474C.795 178.721 56 124.008 123.996 124H1.459C.657 124 0 123.344 0 122.541V1.474C0 .664.664 0 1.474.008c67.274.78 121.669 55.137 122.522 122.387V1.46c0-.803.657-1.46 1.46-1.46h121.07c.81 0 1.474.664 1.467 1.474-.679 58.224-41.486 106.801-96.055 119.367-1.686.386-1.401 2.875.336 2.875h-.008Z"
+            />
+            <defs>
+              <linearGradient
+                id="paint0_linear_2452_11835"
+                x1="218"
+                x2="-47.283"
+                y1="258"
+                y2="153.706"
+                gradientUnits="userSpaceOnUse"
+              >
+                <stop offset=".27" stopColor="#FFD3FD" />
+                <stop offset=".838" stopColor="#806EFF" />
+              </linearGradient>
+              <linearGradient
+                id="paint1_linear_2452_11835"
+                x1="-21.183"
+                x2="223.712"
+                y1="-7.807"
+                y2="329.472"
+                gradientUnits="userSpaceOnUse"
+              >
+                <stop offset=".27" stopColor="#FEC5FB" />
+                <stop offset=".838" stopColor="#00BAE2" />
+              </linearGradient>
+            </defs>
+          </svg>
         </div>
       </div>
 
