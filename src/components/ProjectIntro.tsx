6ea4b6adac36c93411d2237b02807b9113b24b7d
Nickson Ogm <nicksonogm@gmail.com>
2025-08-19 13:43:53 +0300

    already added a cover for my projects page

--- src/components/ProjectIntro.tsx
+++ src/components/ProjectIntro.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import Image from "next/image";
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { ScrollSmoother } from "gsap/ScrollSmoother";
@@ -8,6 +7,7 @@ import React, { useLayoutEffect, useRef } from "react";
 
 function ProjectIntro() {
   const main = useRef<HTMLDivElement>(null);
+  const heroRef = useRef<HTMLDivElement>(null);
 
   useLayoutEffect(() => {
     gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
@@ -17,7 +17,6 @@ function ProjectIntro() {
         wrapper: "#wrapper",
         content: "#content",
         smooth: 2,
-        speed: 3,
         effects: true,
       });
 
@@ -25,66 +24,42 @@ function ProjectIntro() {
         speed: () => gsap.utils.random(0.55, 0.85, 0.05),
       });
 
-      gsap.to(".anim-swipe", {
-        yPercent: 300,
-        delay: 0.2,
-        duration: 3,
-        stagger: {
-          from: "random",
-          each: 0.1,
-        },
-        ease: "sine.out",
-      });
-
-      gsap.to(".hero__image-cont > img", {
-        scale: 1.5,
-        xPercent: 20,
-        scrollTrigger: {
-          trigger: ".hero",
-          start: "top top",
-          end: "+=3000px",
-          scrub: true,
-        },
-      });
-
-      // --- SCROLL REVEAL ANIMATION CODE ---
-      const animateFrom = (elem: HTMLElement, direction: number = 1) => {
-        let x = 0,
-          y = direction * 100;
-        if (elem.classList.contains("gs_reveal_fromLeft")) {
-          x = -100;
-          y = 0;
-        } else if (elem.classList.contains("gs_reveal_fromRight")) {
-          x = 100;
-          y = 0;
-        }
-        elem.style.transform = `translate(${x}px, ${y}px)`;
-        elem.style.opacity = "0";
-        gsap.fromTo(
-          elem,
-          { x, y, autoAlpha: 0 },
-          {
-            duration: 1.25,
-            x: 0,
-            y: 0,
-            autoAlpha: 1,
-            ease: "expo",
-            overwrite: "auto",
-          }
-        );
-      };
-      const hide = (elem: HTMLElement) => {
-        gsap.set(elem, { autoAlpha: 0 });
-      };
-      gsap.utils.toArray<HTMLElement>(".gs_reveal").forEach((elem) => {
-        hide(elem);
-        ScrollTrigger.create({
-          trigger: elem,
-          onEnter: () => animateFrom(elem),
-          onEnterBack: () => animateFrom(elem, -1),
-          onLeave: () => hide(elem),
+      const swipeAnimation = () => {
+        gsap.to(".anim-swipe", {
+          yPercent: 300,
+          duration: 1.5,
+          stagger: {
+            from: "random",
+            each: 0.1,
+          },
+          ease: "power4.out",
         });
-      });
+      };
+
+      swipeAnimation();
+
+      const hero = heroRef.current;
+      if (!hero) return;
+
+      let startY: number;
+      const handleTouchStart = (e: TouchEvent) => {
+        startY = e.touches[0].clientY;
+      };
+
+      const handleTouchEnd = (e: TouchEvent) => {
+        const endY = e.changedTouches[0].clientY;
+        const deltaY = startY - endY;
+        if (deltaY > 50) swipeAnimation();
+      };
+
+      hero.addEventListener("touchstart", handleTouchStart);
+      hero.addEventListener("touchend", handleTouchEnd);
+
+      return () => {
+        hero.removeEventListener("touchstart", handleTouchStart);
+        hero.removeEventListener("touchend", handleTouchEnd);
+        smoother.kill();
+      };
     }, main);
 
     return () => ctx.revert();
@@ -94,40 +69,27 @@ function ProjectIntro() {
     <div ref={main}>
       <div id="wrapper">
         <div id="content">
-          <section className="hero">
-            <div className="hero__inner constrain">
-              <div className="hero__image-cont">
-                <img
-                  src="/images/Nick.jpg"
-                  alt="A portrait split into animated columns."
-                />
-                <div className="anim-swipe"></div>
-              </div>
-              <div className="hero__image-cont">
-                <img src="/images/Nick.jpg" alt="" />
-                <div className="anim-swipe"></div>
-              </div>
-              <div className="hero__image-cont">
-                <img src="/images/Nick.jpg" alt="" />
-                <div className="anim-swipe"></div>
-              </div>
-              <div className="hero__image-cont">
-                <img src="/images/Nick.jpg" alt="" />
-                <div className="anim-swipe"></div>
-              </div>
-              <div className="hero__image-cont">
-                <img src="/images/Nick.jpg" alt="" />
-                <div className="anim-swipe"></div>
-              </div>
-              <div className="hero__image-cont">
-                <img src="/images/Nick.jpg" alt="" />
-                <div className="anim-swipe"></div>
-              </div>
+          <section className="hero" ref={heroRef}>
+            <div className="hero__inner">
+              {[...Array(6)].map((_, i) => (
+                <div key={i} className="hero__image-cont">
+                  <div className="anim-swipe"></div>
+                </div>
+              ))}
             </div>
           </section>
 
           <div className="content-padding">
-            <div className="features">{/* ... your feature items ... */}</div>
+            <div className="features">
+              <div className="features__item">
+                <div className="features__content">
+                  <h2 className="features__title gs_reveal">Project Title</h2>
+                  <p className="features__description gs_reveal">
+                    Project description goes here...
+                  </p>
+                </div>
+              </div>
+            </div>
           </div>
 
           <section className="spacer"></section>
@@ -152,131 +114,125 @@ function ProjectIntro() {
           margin: 0;
           padding: 0;
           overflow-x: hidden;
-          background-color: #111111;
+          background-color: #fffefeff;
           font-weight: 300;
+          font-family: -apple-system, BlinkMacSystemFont, sans-serif;
         }
 
         .hero {
-          height: 100vh;
+          height: 80vh;
+          width: 80vw;
+          margin: 0 auto;
         }
         .hero__inner {
           height: 100%;
           display: grid;
           grid-template-columns: repeat(6, 1fr);
+          width: 100%;
         }
         .hero__image-cont {
           position: relative;
           overflow: hidden;
+          background-color: #fffefeff;
+          background-image: url("images/Nick.jpg");
+          background-size: 600% 100%; /* 6x width for 6 columns */
+          background-repeat: no-repeat;
         }
+
+        /* Corrected background positions */
+        .hero__image-cont:nth-child(1) {
+          background-position: 0% 0;
+        }
+        .hero__image-cont:nth-child(2) {
+          background-position: 20% 0;
+        }
+        .hero__image-cont:nth-child(3) {
+          background-position: 40% 0;
+        }
+        .hero__image-cont:nth-child(4) {
+          background-position: 60% 0;
+        }
+        .hero__image-cont:nth-child(5) {
+          background-position: 80% 0;
+        }
+        .hero__image-cont:nth-child(6) {
+          background-position: 100% 0;
+        }
+
         .hero__image-cont:not(:last-child):after {
           content: "";
           position: absolute;
           right: 0px;
-          background-color: #111111;
+          background-color: #0f0101ff;
           height: 100%;
           top: 0;
           width: 2.5px;
           z-index: 999;
         }
 
-        .hero__image-cont img,
-        .hero__image-cont .anim-swipe {
+        .anim-swipe {
           position: absolute;
-          width: 600%; /* Changed from 700% to match 6 columns */
+          width: 100%;
           height: 100%;
           top: 0;
           left: 0;
-          object-fit: cover;
-        }
-        .anim-swipe {
-          width: 100%;
           background-color: #111111;
+          transform: translateY(0);
+          z-index: 2;
         }
 
-        /* Corrected the left offsets to start from 0 */
-        .hero__image-cont:nth-child(1) img {
-          left: 0%;
-        }
-        .hero__image-cont:nth-child(2) img {
-          left: -100%;
-        }
-        .hero__image-cont:nth-child(3) img {
-          left: -200%;
-        }
-        .hero__image-cont:nth-child(4) img {
-          left: -300%;
-        }
-        .hero__image-cont:nth-child(5) img {
-          left: -400%;
-        }
-        .hero__image-cont:nth-child(6) img {
-          left: -500%;
-        }
-
-        /* --- Other Styles --- */
-        .spacer {
-          height: 100vh;
-        }
         .scroll {
           position: fixed;
           bottom: 1rem;
           left: 50%;
           transform: translateX(-50%);
           z-index: 999;
+          animation: bounce 2s infinite;
         }
+
+        @keyframes bounce {
+          0%,
+          20%,
+          50%,
+          80%,
+          100% {
+            transform: translateX(-50%) translateY(0);
+          }
+          40% {
+            transform: translateX(-50%) translateY(-20px);
+          }
+          60% {
+            transform: translateX(-50%) translateY(-10px);
+          }
+        }
+
         .content-padding {
           max-width: 1240px;
           margin: 0 auto;
-          padding: 1rem;
+          padding: 4rem 1rem;
+          color: white;
         }
-        .features {
-          display: flex;
-          flex-direction: column;
-          gap: 3rem;
-        }
+
         .features__item {
-          display: flex;
-          flex-wrap: wrap;
-          align-items: center;
-          gap: 2rem;
-          height: 100vh;
-          border-top: dashed 2px grey;
+          padding: 2rem 0;
+          border-top: 1px solid #333;
         }
-        .features__item--left {
-          flex-direction: row;
-          text-align: left;
-        }
-        .features__item--right {
-          flex-direction: row-reverse;
-          text-align: right;
-        }
-        .features__image {
-          flex: 1 1 40%;
-          position: relative;
-        }
-        .features__card {
-          border-radius: 8px;
-          overflow: hidden;
-          position: relative;
-          aspect-ratio: 1 / 1;
-        }
-        .features__img {
-          width: 100%;
-          height: 100%;
-          position: absolute;
-          object-fit: cover;
-          display: block;
-        }
-        .features__content {
-          flex: 1 1 55%;
-        }
+
         .features__title {
-          font-size: 1.8em;
-          margin-block-end: 1rem;
+          font-size: 2.5rem;
+          margin-bottom: 1rem;
         }
+
         .features__description {
+          font-size: 1.2rem;
           line-height: 1.6;
+          color: #aaa;
         }
+
+        .spacer {
+          height: 100vh;
+        }
+
         .gs_reveal {
           opacity: 0;
           visibility: hidden;
