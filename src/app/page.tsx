d1f3a1c683b4b346f8013e8c5a15a11900bad32e
Nickson Ogm <nicksonogm@gmail.com>
2025-08-21 15:32:47 +0300

    added the contact form in the projects page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,27 +7,11 @@ import EducationComponent from "@/components/EducationTimeline";
 import WorkExperience from "@/components/WorkExperience";
 import FooterLayout from "@/components/layout/FooterLayout";
 import ContactForm from "@/components/layout/ContactForm";
-import Head from "next/head";
 
 export default function Home() {
   const [isContactFormOpen, setContactFormOpen] = useState(false);
   return (
     <div className="">
-      <Head>
-        <style jsx global>{`
-          .nav-link {
-            position: relative;
-            transition: color 0.3s;
-          }
-          .hamburger-line {
-            display: block;
-            width: 100%;
-            height: 2px;
-            background-color: white;
-            transition: all 0.3s ease-in-out;
-          }
-        `}</style>
-      </Head>
       <HeaderComponent onContactClick={() => setContactFormOpen(true)} />
       <HeroSection />
       <IntroSection />
--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,16 +1,25 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
 import HeaderComponent from "@/components/layout/HeaderComponent";
 import ProjectIntro from "@/components/ProjectIntro";
 import ProjectsComponent from "@/components/ProjectsComponent";
 import FooterLayout from "@/components/layout/FooterLayout";
+import ContactForm from "@/components/layout/ContactForm";
 
 function page() {
+  const [isContactFormOpen, setContactFormOpen] = useState(false);
+
   return (
     <div className="">
-      <HeaderComponent />
+      <HeaderComponent onContactClick={() => setContactFormOpen(true)} />
       <ProjectIntro />
       <ProjectsComponent />
       <FooterLayout />
+      <ContactForm
+        show={isContactFormOpen}
+        onClose={() => setContactFormOpen(false)}
+      />
     </div>
   );
 }
