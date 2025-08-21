20c3f9844d519682760efa6284ae7d473fca3527
Nickson Ogm <nicksonogm@gmail.com>
2025-08-21 17:34:33 +0300

    added a smoth scroller to the services page

--- src/components/ServicesComponent.tsx
+++ src/components/ServicesComponent.tsx
@@ -3,10 +3,10 @@ import React from "react";
 function ServicesComponent() {
   return (
     <>
-      <section className="bg-white dark:bg-gray-900">
+      <section className="bg-white dark:bg-[#F2F2EF]">
         <div className="py-8 px-4 mx-auto max-w-screen-xl sm:py-16 lg:px-6">
           <div className="max-w-screen-md mb-8 lg:mb-16">
-            <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">
+            <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-[#2B6879]">
               Designed for business teams like yours
             </h2>
             <p className="text-gray-500 sm:text-xl dark:text-gray-400">
@@ -30,7 +30,7 @@ function ServicesComponent() {
                   ></path>
                 </svg>
               </div>
-              <h3 className="mb-2 text-xl font-bold dark:text-white">
+              <h3 className="mb-2 text-xl font-bold dark:text-[#2B6879]">
                 Marketing
               </h3>
               <p className="text-gray-500 dark:text-gray-400">
@@ -50,7 +50,9 @@ function ServicesComponent() {
                   <path d="M10.394 2.08a1 1 0 00-.788 0l-7 3a1 1 0 000 1.84L5.25 8.051a.999.999 0 01.356-.257l4-1.714a1 1 0 11.788 1.838L7.667 9.088l1.94.831a1 1 0 00.787 0l7-3a1 1 0 000-1.838l-7-3zM3.31 9.397L5 10.12v4.102a8.969 8.969 0 00-1.05-.174 1 1 0 01-.89-.89 11.115 11.115 0 01.25-3.762zM9.3 16.573A9.026 9.026 0 007 14.935v-3.957l1.818.78a3 3 0 002.364 0l5.508-2.361a11.026 11.026 0 01.25 3.762 1 1 0 01-.89.89 8.968 8.968 0 00-5.35 2.524 1 1 0 01-1.4 0zM6 18a1 1 0 001-1v-2.065a8.935 8.935 0 00-2-.712V17a1 1 0 001 1z"></path>
                 </svg>
               </div>
-              <h3 className="mb-2 text-xl font-bold dark:text-white">Legal</h3>
+              <h3 className="mb-2 text-xl font-bold dark:text-[#2B6879]">
+                Legal
+              </h3>
               <p className="text-gray-500 dark:text-gray-400">
                 Protect your organization, devices and stay compliant with our
                 structured workflows and custom permissions made for you.
@@ -72,7 +74,7 @@ function ServicesComponent() {
                   <path d="M2 13.692V16a2 2 0 002 2h12a2 2 0 002-2v-2.308A24.974 24.974 0 0110 15c-2.796 0-5.487-.46-8-1.308z"></path>
                 </svg>
               </div>
-              <h3 className="mb-2 text-xl font-bold dark:text-white">
+              <h3 className="mb-2 text-xl font-bold dark:text-[#2B6879]">
                 Business Automation
               </h3>
               <p className="text-gray-500 dark:text-gray-400">
@@ -96,7 +98,7 @@ function ServicesComponent() {
                   ></path>
                 </svg>
               </div>
-              <h3 className="mb-2 text-xl font-bold dark:text-white">
+              <h3 className="mb-2 text-xl font-bold dark:text-[#2B6879]">
                 Finance
               </h3>
               <p className="text-gray-500 dark:text-gray-400">
@@ -115,7 +117,7 @@ function ServicesComponent() {
                   <path d="M7 3a1 1 0 000 2h6a1 1 0 100-2H7zM4 7a1 1 0 011-1h10a1 1 0 110 2H5a1 1 0 01-1-1zM2 11a2 2 0 012-2h12a2 2 0 012 2v4a2 2 0 01-2 2H4a2 2 0 01-2-2v-4z"></path>
                 </svg>
               </div>
-              <h3 className="mb-2 text-xl font-bold dark:text-white">
+              <h3 className="mb-2 text-xl font-bold dark:text-[#2B6879]">
                 Enterprise Design
               </h3>
               <p className="text-gray-500 dark:text-gray-400">
@@ -138,7 +140,7 @@ function ServicesComponent() {
                   ></path>
                 </svg>
               </div>
-              <h3 className="mb-2 text-xl font-bold dark:text-white">
+              <h3 className="mb-2 text-xl font-bold dark:text-[#2B6879]">
                 Operations
               </h3>
               <p className="text-gray-500 dark:text-gray-400">
