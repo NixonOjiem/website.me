187dcb721933abfcf122ba26bfdaa93382af8582
Nickson Ogm <nicksonogm@gmail.com>
2025-08-30 09:57:23 +0300

    destructured the service component

---
+++ src/app/data/constants.js
@@ -0,0 +1,7 @@
+export const gradients = [
+  "from-teal-600 to-blue-700",
+  "from-pink-600 to-red-600",
+  "from-purple-600 to-indigo-700",
+  "from-green-600 to-teal-700",
+  "from-yellow-600 to-orange-600",
+];
